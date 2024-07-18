3aa0702e9c987521effac6888204b64fd8db816b
lucaslimal04 <lucasrodrigues_010@hotmail.com>
2024-07-18 08:26:55 -0300

    organo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from './componentes/Banner'
+import Formulario from './componentes/Formulario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+      </div>
+
+  );
+}
+
+export default App
